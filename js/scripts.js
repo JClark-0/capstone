4cda1a257b687ae54a7736b052302bdd97523f27
JClark-0 <clarj524@newschool.edu>
2024-04-12 15:10:55 -0400

    tidy up

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,4 @@
 
-let database = [];
-class locationObject {
-  constructor(latNum, lngNum) {
-    this.latNum = latNum;
-    this.lngNum = lngNum;
-  }
-}
-
-// ------- Reverse Geocoding ------- 
-function getLocation(lat,lng){
-  const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-fetch(`https://us1.locationiq.com/v1/reverse?lat=${lat}&lon=${lng}&format=json&zoom=14&accept-language=en&key=pk.83485c19ebc3983a826192780bdd4e9c`, options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-}
 
 // ------- Latitude & Longitude ------- 
 document.addEventListener('DOMContentLoaded', () => {
@@ -37,7 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 });
 
-// ------- Push data to database ------- 
+// ------- Create lat lng Database -------
+let database = [];
+
+// --- JSON Lat & Lng through API pushed to database ---
 function fetchAndSave(lat,lng){
   let urlPollen = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,us_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,ammonia&hourly=us_aqi,us_aqi_pm2_5,us_aqi_pm10,us_aqi_nitrogen_dioxide,us_aqi_carbon_monoxide,us_aqi_ozone,us_aqi_sulphur_dioxide&timezone=auto&past_hours=1&forecast_days=1&forecast_hours=1`;
   fetch(urlPollen)
@@ -47,70 +33,7 @@ function fetchAndSave(lat,lng){
   })
 }
 
-function renderOnScreen(data){
-  
-  // Clear Pollutant circles 
-  document.querySelectorAll('.pollutants').forEach(box => {
-    box.innerHTML = ''; 
-  });
-
-    //Changes value on variable
-    document.documentElement.style.setProperty('--on_load', 100);
-
-    // ------- Location ------- 
-    let timeZone = document.getElementById('timezone');
-    let formattedTimezone = data.timezone.replace(/_/g, ' ').replace(/[\/]/g, ', ');
-    timeZone.innerHTML = formattedTimezone;
-
-
-    // ------- Calling Pollutant Functions ------- 
-    // createPollutant function: Data, Pollutant Name, Container Id, Pollutant Class
-    // showData function: Data, Pollutant Name, Container Id
-    //PM2.5: 
-    createPollutant (data, 'pm2_5', 'pm2_5_count', 'pm2_5');
-    showData (data, 'pm2_5', 'pm2_5_data' ,'pm2_5UnitId');
-    //Ozone: 
-    createPollutant (data, 'ozone', 'ozone_count', 'ozone');
-    showData (data, 'ozone', 'ozone_data','ozoneUnitId');
-    //Carbon Monoxide:
-    createPollutant (data, 'carbon_monoxide', 'cm_count', 'cm');
-    showData (data, 'carbon_monoxide', 'cm_data','cmUnitId');
-    //Nitrogen Dioxide: 
-    createPollutant (data, 'nitrogen_dioxide', 'nd_count', 'nd');
-    showData (data, 'nitrogen_dioxide', 'nd_data', 'ndUnitId');
-    //Sulphur Dioxide:
-    createPollutant (data, 'sulphur_dioxide', 'sd_count', 'sd');
-    showData (data, 'sulphur_dioxide', 'sd_data','sdUnitId');
-
-    // ------- USA AQI ------- 
-    let aqi = data.current.us_aqi;
-    let aqi_num = document.getElementById('aqi');
-    aqi_num.innerHTML = aqi +' AQI';
-    // console.log('USA AQI', aqi);
-
-    let conditionText = '';
-    if (aqi <= 50){
-      conditionText = 'Good';
-      // console.log('Condition: Good');
-    } else if ( aqi >= 51 && aqi <= 100) {
-      conditionText = 'Moderate';
-      document.documentElement.style.setProperty('--pm2_5', '#187D40')
-      document.documentElement.style.setProperty('--ozone', '#1CB659')
-      document.documentElement.style.setProperty('--cm', '#53D5C5')
-      document.documentElement.style.setProperty('--sd', '#63EC9A')
-      document.documentElement.style.setProperty('--nd', '#9DE6F6')
-    } else if (aqi >= 101 && aqi <= 150) {
-      conditionText = 'Unhealthy(for senstive groups)';
-    } else if (aqi >= 151 && aqi <= 200) {
-      conditionText = 'Unhealthy';
-    } else if (aqi >= 201 && aqi <= 300) {
-      conditionText = 'Very Unhealthy';
-    } else if (aqi >= 301 && aqi <= 500) {
-      conditionText = 'Hazardous';
-    }
-    document.getElementById('condition').textContent = conditionText;
-}
-
+// --- Location added to database ready to render ---
 function fetchData(lat,lng) {
   let urlPollen = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,us_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,ammonia&hourly=us_aqi,us_aqi_pm2_5,us_aqi_pm10,us_aqi_nitrogen_dioxide,us_aqi_carbon_monoxide,us_aqi_ozone,us_aqi_sulphur_dioxide&timezone=auto&past_hours=1&forecast_days=1&forecast_hours=1`;
   fetch(urlPollen)
@@ -119,11 +42,83 @@ function fetchData(lat,lng) {
 
     database.splice(0, 0, data);
     renderOnScreen(database[0]);
-    console.log(data);
   })
   .catch((error) => {
     console.error('Error', error);
   });
+}
+
+// ------- Render data on page -------
+function renderOnScreen(data){
+  
+  // Clear Pollutant circles 
+  document.querySelectorAll('.pollutants').forEach(box => {
+    box.innerHTML = ''; 
+  });
+
+  // Changes value on variable 
+  document.documentElement.style.setProperty('--on_load', 100);
+
+  // ------- Location ------- 
+  let timeZone = document.getElementById('timezone');
+  let formattedTimezone = data.timezone.replace(/_/g, ' ').replace(/[\/]/g, ', ');
+  timeZone.innerHTML = formattedTimezone;
+
+  // ------- Calling Pollutant Functions ------- 
+  // createPollutant function: Data, Pollutant Name, Container Id, Pollutant Class
+  // showData function: Data, Pollutant Name, Container Id
+  //PM2.5: 
+  createPollutant (data, 'pm2_5', 'pm2_5_count', 'pm2_5');
+  showData (data, 'pm2_5', 'pm2_5_data' ,'pm2_5UnitId');
+  //Ozone: 
+  createPollutant (data, 'ozone', 'ozone_count', 'ozone');
+  showData (data, 'ozone', 'ozone_data','ozoneUnitId');
+  //Carbon Monoxide:
+  createPollutant (data, 'carbon_monoxide', 'cm_count', 'cm');
+  showData (data, 'carbon_monoxide', 'cm_data','cmUnitId');
+  //Nitrogen Dioxide: 
+  createPollutant (data, 'nitrogen_dioxide', 'nd_count', 'nd');
+  showData (data, 'nitrogen_dioxide', 'nd_data', 'ndUnitId');
+  //Sulphur Dioxide:
+  createPollutant (data, 'sulphur_dioxide', 'sd_count', 'sd');
+  showData (data, 'sulphur_dioxide', 'sd_data','sdUnitId');
+
+  // ------- USA AQI ------- 
+  let aqi = data.current.us_aqi;
+  let aqi_num = document.getElementById('aqi');
+  aqi_num.innerHTML = aqi +' AQI';
+  // console.log('USA AQI', aqi);
+
+  let conditionText = '';
+  if (aqi <= 50){
+    conditionText = 'Good';
+  } else if ( aqi >= 51 && aqi <= 100) {
+    conditionText = 'Moderate';
+    document.documentElement.style.setProperty('--pm2_5', '#187D40')
+    document.documentElement.style.setProperty('--ozone', '#1CB659')
+    document.documentElement.style.setProperty('--cm', '#53D5C5')
+    document.documentElement.style.setProperty('--sd', '#63EC9A')
+    document.documentElement.style.setProperty('--nd', '#9DE6F6')
+  } else if (aqi >= 101 && aqi <= 150) {
+    conditionText = 'Unhealthy(for senstive groups)';
+  } else if (aqi >= 151 && aqi <= 200) {
+    conditionText = 'Unhealthy';
+  } else if (aqi >= 201 && aqi <= 300) {
+    conditionText = 'Very Unhealthy';
+  } else if (aqi >= 301 && aqi <= 500) {
+    conditionText = 'Hazardous';
+  }
+  document.getElementById('condition').textContent = conditionText;
+}
+
+// ------- Function: Show Location Name ------- 
+function getLocation(lat,lng){
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch(`https://us1.locationiq.com/v1/reverse?lat=${lat}&lon=${lng}&format=json&zoom=14&accept-language=en&key=pk.83485c19ebc3983a826192780bdd4e9c`, options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
 }
 
 // -------  Function: Circles from pollutant count ------- 
@@ -153,7 +148,7 @@ const createPollutant = (data, pollutantName, containerId, pollutantClass) => {
 };
 
 // ------- Function: Show pollutant data ------- 
-  const showData = (data, pollutantName, containerId, unitId) =>{
+const showData = (data, pollutantName, containerId, unitId) =>{
     
     let pollutantData = document.getElementById(containerId);
     pollutantData.innerHTML = data.current[pollutantName];
