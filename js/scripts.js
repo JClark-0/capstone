a7a90b2b9fc05132c8fd773dfd835019439a3417
JClark-0 <clarj524@newschool.edu>
2024-04-12 14:47:09 -0400

    Code clean up

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 
 
 let database = [];
-
 class locationObject {
   constructor(latNum, lngNum) {
     this.latNum = latNum;
@@ -10,7 +9,17 @@ class locationObject {
   }
 }
 
-// Getting your lat and lng based on location saving to variable 
+// ------- Reverse Geocoding ------- 
+function getLocation(lat,lng){
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch(`https://us1.locationiq.com/v1/reverse?lat=${lat}&lon=${lng}&format=json&zoom=14&accept-language=en&key=pk.83485c19ebc3983a826192780bdd4e9c`, options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+}
+
+// ------- Latitude & Longitude ------- 
 document.addEventListener('DOMContentLoaded', () => {
 
   navigator.geolocation.getCurrentPosition((position) => {
@@ -19,8 +28,6 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData(lat, lng); 
   });
 
-  // Button click event to change location
-  // document.getElementById('nxt_btn').addEventListener('click', () => {
   fetch('json/data.json')
       .then(response => response.json())
       .then(data => {
@@ -32,65 +39,58 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   });
 
+// ------- Push data to database ------- 
 function fetchAndSave(lat,lng){
   let urlPollen = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,us_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,ammonia&hourly=us_aqi,us_aqi_pm2_5,us_aqi_pm10,us_aqi_nitrogen_dioxide,us_aqi_carbon_monoxide,us_aqi_ozone,us_aqi_sulphur_dioxide&timezone=auto&past_hours=1&forecast_days=1&forecast_hours=1`;
   fetch(urlPollen)
-  .then((response) => response.json()) // Return it as JSON data
-  .then((data) => { // Lets do stuff with the data now
+  .then((response) => response.json())
+  .then((data) => { 
     database.push(data)
   })
 }
 
 function renderOnScreen(data){
   
-    document.querySelectorAll('.pollutants').forEach(box => {
-      box.innerHTML = '';
-    });
+  // Clear Pollutant circles 
+  document.querySelectorAll('.pollutants').forEach(box => {
+    box.innerHTML = ''; 
+  });
 
+    //Changes value on variable
     document.documentElement.style.setProperty('--on_load', 100);
 
-    // Just to see what is going on
-    // console.log(data); 
-
-    // Location
+    // ------- Location ------- 
     let timeZone = document.getElementById('timezone');
     let formattedTimezone = data.timezone.replace(/_/g, ' ').replace(/[\/]/g, ', ');
     timeZone.innerHTML = formattedTimezone;
 
+
+    // ------- Calling Pollutant Functions ------- 
     // createPollutant function: Data, Pollutant Name, Container Id, Pollutant Class
     // showData function: Data, Pollutant Name, Container Id
-
-    //PM2.5: Create Pollutant + Show Data with units
+    //PM2.5: 
     createPollutant (data, 'pm2_5', 'pm2_5_count', 'pm2_5');
     showData (data, 'pm2_5', 'pm2_5_data' ,'pm2_5UnitId');
-    // showUnit (data, 'pm2_5');
-
-    //Ozone: Create Pollutant + Show Data with units
+    //Ozone: 
     createPollutant (data, 'ozone', 'ozone_count', 'ozone');
     showData (data, 'ozone', 'ozone_data','ozoneUnitId');
-
-    //Carbon Monoxide: Create Pollutant + Show Data with units
+    //Carbon Monoxide:
     createPollutant (data, 'carbon_monoxide', 'cm_count', 'cm');
     showData (data, 'carbon_monoxide', 'cm_data','cmUnitId');
-
-    //Nitrogen Dioxide: Data, Pollutant Name, Container Id, Pollutant Class 
+    //Nitrogen Dioxide: 
     createPollutant (data, 'nitrogen_dioxide', 'nd_count', 'nd');
     showData (data, 'nitrogen_dioxide', 'nd_data', 'ndUnitId');
-
-    //Sulphur Dioxide:Data, Pollutant Name, Container Id, Pollutant Class  
+    //Sulphur Dioxide:
     createPollutant (data, 'sulphur_dioxide', 'sd_count', 'sd');
     showData (data, 'sulphur_dioxide', 'sd_data','sdUnitId');
 
-    //USA AQI
+    // ------- USA AQI ------- 
     let aqi = data.current.us_aqi;
     let aqi_num = document.getElementById('aqi');
     aqi_num.innerHTML = aqi +' AQI';
-
     // console.log('USA AQI', aqi);
 
-
     let conditionText = '';
-
     if (aqi <= 50){
       conditionText = 'Good';
       // console.log('Condition: Good');
@@ -101,19 +101,14 @@ function renderOnScreen(data){
       document.documentElement.style.setProperty('--cm', '#53D5C5')
       document.documentElement.style.setProperty('--sd', '#63EC9A')
       document.documentElement.style.setProperty('--nd', '#9DE6F6')
-      // console.log('Condition: Moderate');
     } else if (aqi >= 101 && aqi <= 150) {
-      conditionText = 'Unhealthy(for senstive groups';
-      // console.log('Condition: Unhealthy(for senstive groups)');
+      conditionText = 'Unhealthy(for senstive groups)';
     } else if (aqi >= 151 && aqi <= 200) {
       conditionText = 'Unhealthy';
-      // console.log('Condition: Unhealthy');
     } else if (aqi >= 201 && aqi <= 300) {
       conditionText = 'Very Unhealthy';
-      // console.log('Condition: Very Unhealthy');
     } else if (aqi >= 301 && aqi <= 500) {
       conditionText = 'Hazardous';
-      // console.log('Condition: Hazardous');
     }
     document.getElementById('condition').textContent = conditionText;
 }
@@ -121,90 +116,19 @@ function renderOnScreen(data){
 function fetchData(lat,lng) {
   let urlPollen = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,us_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,ammonia&hourly=us_aqi,us_aqi_pm2_5,us_aqi_pm10,us_aqi_nitrogen_dioxide,us_aqi_carbon_monoxide,us_aqi_ozone,us_aqi_sulphur_dioxide&timezone=auto&past_hours=1&forecast_days=1&forecast_hours=1`;
   fetch(urlPollen)
-  .then((response) => response.json()) // Return it as JSON data
-  .then((data) => { // Lets do stuff with the data now
+  .then((response) => response.json())
+  .then((data) => {
 
     database.splice(0, 0, data);
     renderOnScreen(database[0]);
     console.log(data);
-
-    // document.documentElement.style.setProperty('--on_load', 100);
-
-    // // Just to see what is going on
-    // // console.log(data); 
-
-    // // Location
-    // let timeZone = document.getElementById('timezone');
-    // let formattedTimezone = data.timezone.replace(/_/g, ' ').replace(/[\/]/g, ', ');
-    // timeZone.innerHTML = formattedTimezone;
-
-    // // createPollutant function: Data, Pollutant Name, Container Id, Pollutant Class
-    // // showData function: Data, Pollutant Name, Container Id
-
-    // //PM2.5: Create Pollutant + Show Data with units
-    // createPollutant (data, 'pm2_5', 'pm2_5_count', 'pm2_5');
-    // showData (data, 'pm2_5', 'pm2_5_data' ,'pm2_5UnitId');
-    // // showUnit (data, 'pm2_5');
-
-    // //Ozone: Create Pollutant + Show Data with units
-    // createPollutant (data, 'ozone', 'ozone_count', 'ozone');
-    // showData (data, 'ozone', 'ozone_data','ozoneUnitId');
-
-    // //Carbon Monoxide: Create Pollutant + Show Data with units
-    // createPollutant (data, 'carbon_monoxide', 'cm_count', 'cm');
-    // showData (data, 'carbon_monoxide', 'cm_data','cmUnitId');
-
-    // //Nitrogen Dioxide: Data, Pollutant Name, Container Id, Pollutant Class 
-    // createPollutant (data, 'nitrogen_dioxide', 'nd_count', 'nd');
-    // showData (data, 'nitrogen_dioxide', 'nd_data', 'ndUnitId');
-
-    // //Sulphur Dioxide:Data, Pollutant Name, Container Id, Pollutant Class  
-    // createPollutant (data, 'sulphur_dioxide', 'sd_count', 'sd');
-    // showData (data, 'sulphur_dioxide', 'sd_data','sdUnitId');
-
-    // //USA AQI
-    // let aqi = data.current.us_aqi;
-    // let aqi_num = document.getElementById('aqi');
-    // aqi_num.innerHTML = aqi +' AQI';
-
-    // // console.log('USA AQI', aqi);
-
-
-    // let conditionText = '';
-
-    // if (aqi <= 50){
-    //   conditionText = 'Good';
-    //   // console.log('Condition: Good');
-    // } else if ( aqi >= 51 && aqi <= 100) {
-    //   conditionText = 'Moderate';
-    //   document.documentElement.style.setProperty('--pm2_5', '#187D40')
-    //   document.documentElement.style.setProperty('--ozone', '#1CB659')
-    //   document.documentElement.style.setProperty('--cm', '#53D5C5')
-    //   document.documentElement.style.setProperty('--sd', '#63EC9A')
-    //   document.documentElement.style.setProperty('--nd', '#9DE6F6')
-    //   // console.log('Condition: Moderate');
-    // } else if (aqi >= 101 && aqi <= 150) {
-    //   conditionText = 'Unhealthy(for senstive groups';
-    //   // console.log('Condition: Unhealthy(for senstive groups)');
-    // } else if (aqi >= 101 && aqi <= 150) {
-    //   conditionText = 'Unhealthy';
-    //   // console.log('Condition: Unhealthy');
-    // } else if (aqi >= 201 && aqi <= 300) {
-    //   conditionText = 'Very Unhealthy';
-    //   // console.log('Condition: Very Unhealthy');
-    // } else if (aqi >= 301 && aqi <= 500) {
-    //   conditionText = 'Hazardous';
-    //   // console.log('Condition: Hazardous');
-    // }
-    // document.getElementById('condition').textContent = conditionText;
   })
   .catch((error) => {
     console.error('Error', error);
   });
 }
 
-
-//Function to create the circles based on pollutant count
+// -------  Function: Circles from pollutant count ------- 
 const createPollutant = (data, pollutantName, containerId, pollutantClass) => {
   const pollutantCount = data.current[pollutantName];
   const pollutantCountElement = document.getElementById(containerId);
@@ -230,7 +154,7 @@ const createPollutant = (data, pollutantName, containerId, pollutantClass) => {
     }
   };
 
-//Function to show data 
+// ------- Function: Show pollutant data ------- 
   const showData = (data, pollutantName, containerId, unitId) =>{
     
     let pollutantData = document.getElementById(containerId);
@@ -240,7 +164,7 @@ const createPollutant = (data, pollutantName, containerId, pollutantClass) => {
     pollutantUnit.innerHTML = data.current_units[pollutantName];
   };
 
-
+// ------- Click to Next Location ------- 
 const nextLocation = document.getElementById('nxt_btn');
 let currentLocationIndex = 0;
 nextLocation.onclick = () => {
@@ -252,18 +176,6 @@ nextLocation.onclick = () => {
   console.log(currentLocationIndex);
   renderOnScreen(database[currentLocationIndex]);
 }
-
-
-
-// lat and lng for other pages 
-// if (!document.URL.includes("country.html")) {
-//   lat = 40.014984;
-//   lng = -105.270546;
-
-//   // Call function to fetch air quality data
-//   fetchData(lat, lng);
-// }
-
 
 
 // add saved lat and lng to add to list of places 
