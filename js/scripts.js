395a0873ae3673b21c0447cbd747567625de5cb6
JClark-0 <clarj524@newschool.edu>
2024-04-23 22:29:36 -0400

    popup close fixed

--- js/scripts.js
+++ js/scripts.js
@@ -176,16 +176,23 @@ const expandPollutant = (infoBoxId, expandedId) => {
     const pollutantExpanded = document.getElementById(expandedId);
     const infoBox = document.getElementById(infoBoxId)
     const closePopup = document.querySelectorAll('.close_expanded_pol');
+    // const resetPopup = () => {
+
+    // }
 
     infoBox.onclick = () => {
-      if (pollutantExpanded.style.display === 'none' || pollutantExpanded.style.display === '') { 
+      if (pollutantExpanded.style.display === 'none' ) { 
+        // resetPopup();
+        document.querySelectorAll('.pollutant_expanded').forEach(element => {
+          element.style.display='none';
+        })
         pollutantExpanded.style.display = 'block';
       } else {
         pollutantExpanded.style.display = 'none';
       }
 
-      closePopup.forEach(img => { 
-        img.onclick = () => {
+      closePopup.forEach(icon => { 
+        icon.onclick = () => {
           pollutantExpanded.style.display = 'none';
         }
       });
